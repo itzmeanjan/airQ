1ed33096521c559a9f010c8ba81e6c6366818554
Anjan Roy <anjanroy@yandex.com>
2019-11-30 10:19:22 +0530

    wrote url generator, will keep generating urls, when iterated over

--- airQ/model/url.py
+++ airQ/model/url.py
@@ -3,10 +3,12 @@
 from __future__ import annotations
 from json import load
 from os.path import dirname, abspath, join
+from requests import get
 
 class RequestURL:
-    def __init__(self, config: str = abspath(join(dirname(__file__), '../config.json'))):
+    def __init__(self, config: str = abspath(join(dirname(__file__), '../config.json')), total: int = 10):
         self._config = config
+        self._total = total
         with open(self._config) as fd:
             _data = load(fd)
             self._base = _data['url']
@@ -14,14 +16,29 @@ class RequestURL:
             self._format = _data['format']
             self._offset = str(_data['offset'])
             self._limit = str(_data['limit'])
+        self._setTotal()
 
     @property
-    def getURL(self) -> str:
+    def _getTotal(self) -> int:
+        return get(self._url).json().get('total')
+
+    def _setTotal(self):
+        self._total = self._getTotal
+
+    @property
+    def _url(self) -> str:
         return self._base + '?' + 'api-key=' + self._api_key + '&format=' + self._format + '&offset=' + self._offset + '&limit=' + self._limit
 
-    def updateOffset(self, offset: int) -> RequestURL:
+    def _updateOffset(self, offset: int) -> RequestURL:
         self._offset = str(offset)
         return self
+
+    def __iter__(self):
+        while int(self._offset) < self._total:
+            yield self._url
+            if int(self._updateOffset((int(self._offset) + 10) % self._total)._offset) == 0:
+                break
+
 
 
 if __name__ == "__main__":
